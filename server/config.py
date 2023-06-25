866cbd51c0428fa5addf0408cbb12deede350c0c
Evan Roberts <eroberts1998@icloud.com>
2023-06-24 21:05:17 -0400

    fix typos and ran flask db revision / upgrade head... starting scraper code

--- server/config.py
+++ server/config.py
@@ -1,6 +1,3 @@
-# Standard library imports
-
-# Remote library imports
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -8,15 +5,11 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-# Local imports
-
-# Instantiate app, set attributes
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-# Define metadata, instantiate db
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
@@ -24,8 +17,5 @@ db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 db.init_app(app)
 
-# Instantiate REST API
 api = Api(app)
-
-# Instantiate CORS
 CORS(app)
