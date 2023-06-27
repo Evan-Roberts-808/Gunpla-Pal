a54dc4b17a2698aaec8f83bdd8d9d7d0d834982f
Ethan Beneroff <ethanbeneroff@gmail.com>
2023-06-27 10:55:16 -0400

    flask login implemented

--- server/app.py
+++ server/app.py
@@ -1,12 +1,18 @@
 from flask_migrate import Migrate
 from flask import Flask, request, session
-
+from flask_login import LoginManager, login_user, logout_user, login_required
 from config import app, db, api, Resource
 # from config import Config, app, db, api, Resource
 from models import Gunpla, User, Collection, Wishlist, Theme
 
 
 migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 class GunplasByGrade(Resource):
@@ -19,7 +25,9 @@ class GunplasByGrade(Resource):
 
 api.add_resource(GunplasByGrade, '/gunplas/<string:grade>')
 
+
 class UserProfile(Resource):
+    @login_required
     def get(self, username):
         try:
             user = User.query.filter(User.username == username).first().to_dict()
@@ -30,6 +38,7 @@ class UserProfile(Resource):
 api.add_resource(UserProfile, '/users/<string:username>')
 
 class CollectionsByUser(Resource):
+    @login_required
     def get(self, username):
         try:
             collections = [c.to_dict() for c in Collection.query.filter(Collection.user.username == username).all()]
@@ -40,6 +49,17 @@ class CollectionsByUser(Resource):
 api.add_resource(CollectionsByUser, '/<string:username>/collections')
 #'/<string:username>/collections
 #user profile route '/<string:username>
+
+class WishlistsByUser(Resource):
+    @login_required
+    def get(self, username):
+        try:
+            wishlists = [c.to_dict() for c in Wishlist.query.filter(Wishlist.user.username == username).all()]
+            return wishlists, 200
+        except:
+            return {'error': 'user not found'}, 404
+
+api.add_resource(WishlistsByUser, '/<string:username>/wishlists')
 
 class Signup(Resource):
     def post(self):
@@ -59,6 +79,30 @@ class Signup(Resource):
         return new_user.to_dict(), 201
     
 api.add_resource(Signup, '/signup')
+
+class Login(Resource):
+    
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        
+        user = User.query.filter(User.username == username).first()
+        
+        if user:
+            if user.authenticate(password):
+                login_user(user, remember=True)
+                return user.to_dict(), 200
+        return {'error': '401 Unauthorized'}, 401
+    
+api.add_resource(Login, '/login')
+
+@app.route("/logout", methods = ["POST"])
+@login_required
+def logout():
+    logout_user()
+    #return redirect(url_for('login'))
+    return f'You have logged out. Goodbye'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
