927f34bb0b2dce340963b799a70bc93b4b3467f6
Evan <eroberts1998@icloud.com>
2023-06-28 17:57:05 -0400

    added text

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FaDatabase, FaClipboardList, FaHeart } from 'react-icons/fa';
 
 const Home = () => {
   return (
     <div>
       <div className="jumbotron jumbotron-fluid">
-        <div className="container">
+        <Container>
           <h2>Welcome to GunplaPAL!</h2>
           <p>Explore our database + manage your collections & wishlists.</p>
-        </div>
+          </Container>
       </div>
+      <Container>
+        <Row>
+        <Col md={4}>
+          <div className="feature-box">
+            <FaDatabase className="feature-icon" />
+            <h3>Extensive Database</h3>
+            <p>Discover a vast collection of Gunpla kits, including grades, series, release dates, and more.</p>
+          </div>
+        </Col>
+        <Col md={4}>
+          <div className="feature-box">
+            <FaClipboardList className="feature-icon" />
+            <h3>Manage Collections</h3>
+            <p>Effortlessly organize and track your Gunpla collection, complete with detailed information and images.</p>
+          </div>
+        </Col>
+        <Col md={4}>
+          <div className="feature-box">
+            <FaHeart className="feature-icon" />
+            <h3>Create Wishlists</h3>
+            <p>Keep track of Gunpla kits you desire by creating personalized wishlists with notes and images.</p>
+          </div>
+        </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
